29d0e54790fb5160bd35a2b72f9685d86b9bca22
sya-ri <git@s7a.dev>
2022-12-16 09:52:01 +0900

    refactor: Fix null annotation

--- src/main/java/dev/s7a/base64/Base64ItemStack.java
+++ src/main/java/dev/s7a/base64/Base64ItemStack.java
@@ -28,6 +28,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import java.io.ByteArrayInputStream;
@@ -37,7 +38,7 @@ public class Base64ItemStack {
     private Base64ItemStack() {
     }
 
-    public static @NotNull String encode(@NotNull ItemStack item) throws Base64ConvertException {
+    public static @NotNull String encode(@Nullable ItemStack item) throws Base64ConvertException {
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
              BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream)) {
             dataOutput.writeObject(item);
@@ -47,7 +48,7 @@ public class Base64ItemStack {
         }
     }
 
-    public static @NotNull ItemStack decode(@NotNull String base64) throws Base64ConvertException {
+    public static @Nullable ItemStack decode(@NotNull String base64) throws Base64ConvertException {
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decode(base64));
              BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream)) {
             return (ItemStack) dataInput.readObject();
